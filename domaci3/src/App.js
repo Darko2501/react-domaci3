4be8f2a577181688113b9e9d63d843e0560b9bef
Darko Tokanovic <88401229+Darko2501@users.noreply.github.com>
2024-06-29 19:06:41 +0200

    Add files via upload
    domaci treci react

---
+++ domaci3/src/App.js
@@ -0,0 +1,20 @@
+import Temperature from'./Components/Temperature.js'
+import React, { useState } from 'react';
+function App() {
+  
+ 
+  return (
+    <>
+    <h1>Weather forecast</h1>
+    
+
+  
+
+    <Temperature/>;
+
+    
+    </>
+  );
+}
+
+export default App;
---
+++ domaci3/src/Components/Temperature.js
@@ -0,0 +1,68 @@
+import React,{useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Temperature(){
+  let[state,setState]=useState('');
+  let[city,setCity]=useState('');
+  let[temp,setTemp]=useState('');
+  let[sites,newSites]=useState([]);
+  function addNew(e){
+    e.preventDefault();
+    const newSite={state,city,temp};
+    newSites([...sites,newSite])
+  };
+    return(
+        <>
+        <div className="container">
+        
+              <table  className="table table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col">Country</th>
+                    <th scope="col">City</th>
+                    <th scope="col">Temperature</th>
+                  </tr>
+                </thead>
+                <tbody>
+                 {sites.map((site, index) => (
+              <tr key={index}>
+                <td>{site.state}</td>
+                <td>{site.city}</td>
+                <td>{site.temp} °C</td>
+              </tr>
+            ))}
+          </tbody>
+              </table>
+            
+          <h1>Add new sites:</h1>
+          
+          <form>
+          
+            <div className="mb-3">
+              <label htmlFor="stateInput" className="form-label">State:</label>
+              <input onInput={(e)=>setState(e.target.value)} type="text" className="form-control" id="stateInput" aria-describedby="emailHelp" />
+          
+            </div>
+            <div className="mb-3">
+              <label htmlFor="cityInput" className="form-label">City</label>
+              <input onInput={(e)=>setCity(e.target.value)} type="text" className="form-control" id="cityInput" />
+            </div>
+            <div className="mb-3">
+              <label className="form-check-label" htmlFor="inputTemperature">Temperature:</label>
+              <input onInput={(e)=>setTemp(e.target.value)} type="text" className="form-control" id="inputTemperature" />
+          
+            </div>
+            <div className="d-grid gap-2">
+            <button onClick={addNew} type="submit" className="btn btn-primary">ADD</button>
+  
+            </div>
+            
+          </form>
+        </div>
+
+        </>
+    )
+
+}
+
+export default Temperature;
